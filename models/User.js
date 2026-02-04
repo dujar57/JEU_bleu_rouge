b9ea406481ec25edfd4ceb374b287694cd787ed4
dujar57 <dujar57@users.noreply.github.com>
2026-02-04 17:50:32 +0100

    🐛 Fix: middleware bcrypt sans next() pour Mongoose moderne

--- models/User.js
+++ models/User.js
@@ -48,16 +48,11 @@ const userSchema = new mongoose.Schema({
 });
 
 // Hasher le mot de passe avant de sauvegarder
-userSchema.pre('save', async function(next) {
-  if (!this.isModified('password')) return next();
+userSchema.pre('save', async function() {
+  if (!this.isModified('password')) return;
   
-  try {
-    const salt = await bcrypt.genSalt(10);
-    this.password = await bcrypt.hash(this.password, salt);
-    next();
-  } catch (error) {
-    next(error);
-  }
+  const salt = await bcrypt.genSalt(10);
+  this.password = await bcrypt.hash(this.password, salt);
 });
 
 // Méthode pour comparer les mots de passe
